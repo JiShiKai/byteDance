a1815997aa94790450ead97adfd53e0838d4587c
JiShiKai <49060598+JiShiKai@users.noreply.github.com>
2019-04-08 08:47:23 +0800

    吉世恺改

--- js/topnav.js
+++ js/topnav.js
@@ -1,8 +1,8 @@
 $(function(){
-	$('#nav_bar>li').mouseover(function(){
+	$('#nav_bar>a').mouseover(function(){
 		$(this).css('color','#008cff').siblings().css('color','black');
 	})
-	$('#nav_bar>li').click(function(){
+	$('#nav_bar>a').click(function(){
 		$(this).css('color','#008cff').siblings().css('color','black');
 	})
 })
