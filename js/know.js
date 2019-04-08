b6e2ced200172a587215b97d785b353030188da0
missguo804652369 <804652369@qq.com>
2019-04-08 08:37:00 +0800

    郭泽改
    郭泽改

--- js/know.js
+++ js/know.js
@@ -2,7 +2,7 @@ $(function(){
 $('#con_ul li').click(function (){		
 		var ind = $(this).index();
 			$(this).attr({'class':'con_li_l'}).siblings('li').attr({'class':''})
-		$('#con #con_r').eq(ind).show().siblings('#con_r').hide();		
+		$('#con #con_r').eq(ind).show().siblings('#con_r').hide();	
 	})
 	
 })
